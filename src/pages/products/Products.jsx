0aa600c94e9a0a0575af04d077f26dc5299e20a2
AniBukhrashvili <ana.bukhrashvili.1@btu.edu.ge>
2023-02-24 21:53:00 +0400

    remove gh-pages

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -65,7 +65,7 @@ const Products = () => {
   };
 
   return (
-    <Box sx={{ backgroundColor: "#e3e8e3" }}>
+    <Box>
       <NavBar />
       <Grid container spacing={2} justifyContent="center" display="flex">
         <Grid container item md={1} xs={12} mt="35px">
